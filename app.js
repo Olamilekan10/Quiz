e293873bccba5349f96ea85c2739c515ae09c2de
olamilekan <issaabubakry@gmail.com>
2022-06-04 12:58:48 +0100

    Then query selected the class quiz form

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const correctAnswers = ['A','B','A','A','B','A','A','B','A','B','B','B','A','A','B','A','A','B','A','A'];
 const form = document.querySelector('.quiz-form');
-const result = document.querySelector('.result');
+// const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
